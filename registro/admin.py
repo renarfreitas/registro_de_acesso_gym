022f67938fd726ba27a92b1c4682b810df916eb2
Renar Freitas <renarfreitas@gmail.com>
2025-01-29 14:22:48 -0300

    Criação de Modelos

--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,18 @@
+#from .models import Colaborador, ColetaFaces, Treinamento
 from django.contrib import admin
+from registro.models import (
+    Colaborador, ColetaFaces, Treinamento)
 
-# Register your models here.
+class ColetaFacesInline(admin.StackedInline):
+    model = ColetaFaces
+    extra = 0
+
+class ColaboradorAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+    inlines = (ColetaFacesInline,)
+
+admin.site.register(Colaborador, ColaboradorAdmin)
+
+# admin.site.register(Colaborador)
+# admin.site.register(ColetaFaces)
+admin.site.register(Treinamento)
